f12b73ecfc77c9d512476cdc3bca8cd9777d455d
Brian Kropff <brian.kropff@gmail.com>
2015-04-10 10:06:49 -0700

    Add the basic html to check a triangle

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,9 @@ function triTest(side1, side2, side3) {
   var result;
   if(side1 === 0 || side2 === 0 || side3 === 0){
     result = false;
-  } else {
+  }else if( side1 >= side2 + side3 || side2 >= side1 + side3 || side3 >= side2 + side1){
+    result = "Not a triangle";
+  }else{
     if(side1 === side2 ){
       equalCount ++
     }
@@ -16,14 +18,34 @@ function triTest(side1, side2, side3) {
       equalCount ++
     }
 
-  }//closes first if
+    if(equalCount === 0){
+      result = 'scalene';
 
-    if(equalCount === 1){
+    }else if(equalCount === 1){
       result = 'isosceles';
 
     }else if(equalCount === 3){
       result = 'equilateral';;
     }
-    return result;
+
+  }//closes first if
+
+  return result;
 
 }//close function
+
+$(document).ready(function() {
+  $("form#side-input").submit(function(event) {
+    var a = parseInt($("input#side1").val());
+    var b = parseInt($("input#side2").val());
+    var c = parseInt($("input#side3").val());
+
+    var triangle = triTest(a,b,c);
+    $("#result").text(triangle);
+
+    $("#result").show();
+
+    event.preventDefault();
+
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -8,10 +8,18 @@ describe('triTest', function(){
   });
 
   it("will take (4,1,1) and return 'isosceles'", function(){
-  expect(triTest(4,1,1)).to.equal("isosceles");
+  expect(triTest(4,3,3)).to.equal("isosceles");
   });
 
   it("will take (4,4,4) and return 'equilateral'", function(){
   expect(triTest(4,4,4)).to.equal("equilateral");
+  });
+
+  it("will take (1,1,4) and return 'Not a triangle'", function(){
+  expect(triTest(1,1,4)).to.equal("Not a triangle");
+  });
+
+  it("will take (3,4,5) and return 'scalene'", function(){
+  expect(triTest(3,4,5)).to.equal("scalene");
   });
 });
